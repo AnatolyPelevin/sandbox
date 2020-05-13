2fec556a9a20ac333e8094ce6055dfa690abdfa7
Ilnar.Sayfutdinov <ilnar.sayfutdinov@nordigy.ru>
2020-05-13 17:18:09 +0300

    data-5466-SFDC-data-leaks-in-demand_funnel__c

--- migrations/hive_partitions/migrate.sh
+++ migrations/hive_partitions/migrate.sh
@@ -1,39 +1,26 @@
 #!/bin/bash
 
-prod_env="PROD"
-stage_env="STAGE"
+set -ex
 
-START_DATE=$2
-END_DATE=$3
-TARGET_PARTITION=$4
-
-if [[ "$1" == "$prod_env" ]] ; then
-hive_schema_name="sfdc_production"
-outDataPath="hdfs://nameservice1/envs/production/SFDC/out-data/SFDC_ETL_postgres"
-impalaUrl="http://impala_invalidate:A53d21e6f22@sjc01-c01-hdc05.c01.ringcentral.com:8080"
-beelineUrl="jdbc:hive2://hiveserver.ringcentral.com:10000/default;principal=hive/_HOST@RINGCENTRAL.COM"
-elif [[ "$1" == "$stage_env" ]] ; then
-hive_schema_name="sfdc_release"
-outDataPath="hdfs://nameservice1/envs/release/SFDC/out-data/SFDC_ETL_postgres"
-impalaUrl="http://impala_invalidate:A53d21e6f22@sjc06-c01-hdc13.ops.ringcentral.com:8080"
-beelineUrl="jdbc:hive2://hiveserver.ops.ringcentral.com:10000/default;principal=hive/_HOST@RINGCENTRAL.COM"
-else
-hive_schema_name="sfdc_dev_hf_1_1_4"
-outDataPath="hdfs://nameservice1/envs/dev/SFDC/hf-1.1.4/out-data/SFDC_ETL_postgres"
-impalaUrl="http://admin:password@bda01-t01-hdc03.lab.nordigy.ru:8080"
-beelineUrl="jdbc:hive2://bda01-t01-hdc02:10000/default;principal=hive/_HOST@LAB.NORDIGY.RU"
-fi
-hive_table_name="demand_funnel__c"
+START_DATE=$1
+END_DATE=$2
+targetPartition=$3
+hiveSchemaName=$4
+hiveTableName=$5
+outDataPath=$6
+iisUrl=$7
+beelineUrl=$8
 
 loopEndDate=$(date -I -d "$START_DATE - 1 day")
 d=$(date -I -d "$END_DATE")
+
 while [ "$d" != "$loopEndDate" ]; do
   echo "Processing date: $d"
-  hdfs dfs -mkdir $outDataPath/$hive_table_name/dt=$d
-  hadoop fs -cp $outDataPath/$hive_table_name/dt=$TARGET_PARTITION/* $outDataPath/$hive_table_name/dt=$d
+  hdfs dfs -mkdir $outDataPath/$hiveTableName/dt=$d
+  hadoop fs -cp $outDataPath/$hiveTableName/dt=$targetPartition/* $outDataPath/$hiveTableName/dt=$d
   d=$(date -I -d "$d - 1 day")
 done
 echo "Run msck refresh table"
-beeline -u $beelineUrl --hivevar hive_schema_name=$hive_schema_name --hivevar hive_table_name=$hive_table_name -f hive.sql
+beeline -u $beelineUrl --hivevar hive_schema_name=$hiveSchemaName --hivevar hive_table_name=$hiveTableName -f refresh_hive_table.sql
 echo "Send request to invalidate metadata"
-curl -X POST $impalaUrl/invalidate/$hive_schema_name/$hive_table_name
+curl -X POST $iisUrl/invalidate/$hiveSchemaName/$hiveTableName
