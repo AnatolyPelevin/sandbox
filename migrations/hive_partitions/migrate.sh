81e5aecf95caf9d1a41d51246ddb7b2c8de01a78
Ilnar.Sayfutdinov <ilnar.sayfutdinov@nordigy.ru>
2020-05-27 12:37:38 +0300

    data-5646-Migration-of-demand_funnel__c-hangs-until-you-push-Enter

--- migrations/hive_partitions/migrate.sh
+++ migrations/hive_partitions/migrate.sh
@@ -4,23 +4,24 @@ set -ex
 
 START_DATE=$1
 END_DATE=$2
-targetPartition=$3
+sourcePartition=$3
 hiveSchemaName=$4
 hiveTableName=$5
 outDataPath=$6
 iisUrl=$7
 beelineUrl=$8
 
-loopEndDate=$(date -I -d "$START_DATE - 1 day")
-d=$(date -I -d "$END_DATE")
+
+d=$(date -I -d "$START_DATE")
+loopEndDate=$(date -I -d "$END_DATE + 1 day")
 
 while [ "$d" != "$loopEndDate" ]; do
   echo "Processing date: $d"
-  hdfs dfs -mkdir $outDataPath/$hiveTableName/dt=$d
-  hadoop fs -cp $outDataPath/$hiveTableName/dt=$targetPartition/* $outDataPath/$hiveTableName/dt=$d
-  d=$(date -I -d "$d - 1 day")
+  hadoop fs -mkdir $outDataPath/$hiveTableName/dt=$d
+  hadoop fs -cp $outDataPath/$hiveTableName/dt=$sourcePartition/* $outDataPath/$hiveTableName/dt=$d
+  d=$(date -I -d "$d + 1 day")
 done
-echo "Run msck refresh table"
+echo "Run refresh table"
 beeline -u $beelineUrl --hivevar hive_schema_name=$hiveSchemaName --hivevar hive_table_name=$hiveTableName -f refresh_hive_table.sql
 echo "Send request to invalidate metadata"
 curl -X POST $iisUrl/invalidate/$hiveSchemaName/$hiveTableName
