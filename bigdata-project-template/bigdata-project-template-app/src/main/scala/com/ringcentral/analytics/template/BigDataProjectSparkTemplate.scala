557feb584c36861837f13157cce46710cdefb72a
Roman Rumyantsev <roman.rumyantsev@nordigy.ru>
2020-07-29 11:35:29 +0300

    DATA-5918 optimize pom dependencies and versions, switch on sfd4j-api logging, switch on beeline usage in ansible scenarios

--- bigdata-project-template/bigdata-project-template-app/src/main/scala/com/ringcentral/analytics/template/BigDataProjectSparkTemplate.scala
+++ bigdata-project-template/bigdata-project-template-app/src/main/scala/com/ringcentral/analytics/template/BigDataProjectSparkTemplate.scala
@@ -1,15 +1,18 @@
 package com.ringcentral.analytics.template
 
-import org.apache.spark.internal.Logging
 import org.apache.spark.sql.SparkSession
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 
 
-object BigDataProjectSparkTemplate extends Logging {
+object BigDataProjectSparkTemplate {
+
+    private val log: Logger = LoggerFactory.getLogger(this.getClass.getName.stripSuffix("$"))
 
     def main(args: Array[String]): Unit = {
         implicit val spark: SparkSession = SparkSession.builder
             .enableHiveSupport()
             .getOrCreate()
-        logInfo("This is a BigData Project Spark Template")
+        log.info("This is a BigData Project Spark Template")
     }
 }
